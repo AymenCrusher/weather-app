bf077cd45d5b93365f30b1b513059dabfca47ba1
Ayamun Crusher <ouer.aymen@gmail.com>
2020-02-18 10:45:38 +0100

    updated data provided by the app

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     const url= `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiY3J1aHNlcm8iLCJhIjoiY2s2bDk1YmdoMGJwOTNqcWdlajJpeHh4cyJ9.h6WE-5wcE3gFp80_dfNS-A&limit=1`
-    request ({ url, json: true}, (error, {body}) => {
+    request ({ url, json: true}, (error, {body}={}) => {
         if (error){
             callback('Unable to connect to geolocation service!', undefined)
         } else if (body.message || body.features.length === 0){
